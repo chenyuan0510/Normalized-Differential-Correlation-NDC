6325e1c66220394ba961f56e4d0a1e794c1de1e4
chenyuan0510 <30547734+chenyuan0510@users.noreply.github.com>
2019-06-27 17:42:38 +0800

    Create how to using the NDC_index.m

---
+++ how to using the NDC_index.m
@@ -0,0 +1,5 @@
+1） MATLAB is the tool of NDC;
+2)  Make sure that c++ has installed in your computer;
+3)  Running program “make.m” to compile the getsuper2var and getmutualI2var_fix4;
+4)  Running program NDC_index to calculate NDC and get the oder of each feature;
+    [NDC_score,ordered_index,threshold_mic]=NDC_index(data,num_ndc)
